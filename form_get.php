53d447fcf05bffc8b102dad968f3f6513350321a
senbagaraman <senbagaraman7330@gmail.com>
2025-01-05 22:36:23 +0530

    The form get file is commited in this version

---
+++ form_get.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Example.com</title>
+</head>
+<body>
+    <!-- Form -->
+    <form action="form_get.php" method="get">
+        <!-- Username field -->
+        <label for="username">Username</label><br>
+        <input type="text" name="username"><br>
+        <!-- Password field -->
+         <label for="password">Password</label><br>
+         <input type="password" name="password"><br>
+         <button>submit</button>
+    </form>
+<pre>
+<?php
+        // get the value from the form
+        $user = $_GET['username']; //username
+        $pass = $_GET['password']; //password
+
+        //print the value in the same page
+      printf("Username is:{$user}\n");
+      printf("Password is:{$pass}\n");
+    ?>
+</pre>
+</body>
+</html>
